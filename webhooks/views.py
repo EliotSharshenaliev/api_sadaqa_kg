bf71000b58028d068e4d1b6b4a39f5cae14760ac
Marat Sharshenaliev <sharshenaliev.03@gmail.com>
2023-10-05 22:36:44 +0600

    Изменение в двух endpoint для webhook ков
    добавлены условные конструкции для типа

--- webhooks/views.py
+++ webhooks/views.py
@@ -3,22 +3,26 @@ from rest_framework import generics
 from rest_framework.response import Response
 
 
-class CustomerWebhookView(generics.CreateAPIView):
+class CustomerCreatedWebhookView(generics.CreateAPIView):
     def create(self, request, *args, **kwargs):
         try:
-            customer_data = request.data["data"]["object"]
-            Customer.sync_from_stripe_data(customer_data)
+            event = request.data
+            if event['type'] == 'customer.created':
+                customer_data = event["data"]["object"]
+                Customer.sync_from_stripe_data(customer_data)
         except KeyError or Exception:
             return Response(400)
 
         return Response(200)
 
 
-class SubscriptionWebhookView(generics.CreateAPIView):
+class SubscriptionCreatedWebhookView(generics.CreateAPIView):
     def create(self, request, *args, **kwargs):
         try:
-            subscription_data = request.data["data"]["object"]
-            Subscription.sync_from_stripe_data(subscription_data)
+            event = request.data
+            if event['type'] == 'customer.subscription.created':
+                customer_data = event["data"]["object"]
+                Subscription.sync_from_stripe_data(customer_data)
         except KeyError or Exception:
             return Response(400)
 
