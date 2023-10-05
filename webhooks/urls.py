571046af98ca56ee0696cdb1f288f44ee8e66050
Marat Sharshenaliev <sharshenaliev.03@gmail.com>
2023-10-05 22:33:52 +0600

    Созданы шаблоны для webhook  - customer-created  - subscription-created

--- webhooks/urls.py
+++ webhooks/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from webhooks.views import CustomerWebhookView, SubscriptionWebhookView
+from webhooks.views import CustomerCreatedWebhookView, SubscriptionCreatedWebhookView
 
 urlpatterns = [
-    path(r'customer/', CustomerWebhookView.as_view()),
-    path(r'subscription/', SubscriptionWebhookView.as_view()),
+    path(r'customer-created/', CustomerCreatedWebhookView.as_view()),
+    path(r'subscription-created/', SubscriptionCreatedWebhookView.as_view()),
 ]
