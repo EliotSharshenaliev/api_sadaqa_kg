21147fa4df638662764a675f99a5aad5f9c566cc
Eliot Alderson <eliotalderson_host_35@venom.com>
2023-10-09 04:05:52 +0600

    added user api endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,26 @@
+import logging
+
 from django.contrib.auth import get_user_model
-from rest_framework import status
-from rest_framework.generics import GenericAPIView, RetrieveUpdateAPIView
+from rest_framework import status, mixins
+from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from accounts import serializers
+from accounts.serializers import UserSerializer
 
 User = get_user_model()
+logger = logging.getLogger(__name__)
 
 
-class UserRegisterationAPIView(GenericAPIView):
+class UserRegistrationAPIView(GenericAPIView):
     """
     An endpoint for the client to create a new User.
     """
 
     permission_classes = (AllowAny,)
-    serializer_class = serializers.UserRegisterationSerializer
+    serializer_class = serializers.UserRegistrationSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -58,5 +62,31 @@ class UserLogoutAPIView(GenericAPIView):
             token.blacklist()
             return Response(status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
-            print(e)
+            logger.error("Error: %s" % e.args)
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserAPIView(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericAPIView
+):
+    """
+    Concrete view for retrieving, updating or deleting a user model instance.
+    """
+
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+    def get_object(self):
+        return self.request.user
