bd27c8d6efbc47f5dc6a40d88585499a26bf2bf6
Eliot Alderson <eliotalderson_host_35@venom.com>
2023-10-08 22:41:38 +0600

    changed wrong class name to correct

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from accounts import views
 
 
 urlpatterns = [
-    path("register/", views.UserRegisterationAPIView.as_view(), name="create-user"),
+    path("register/", views.UserRegistrationAPIView.as_view(), name="create-user"),
     path("login/", views.UserLoginAPIView.as_view(), name="login-user"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
 ]
