702fc0dcbf9030dc3a793b740520d0db10f6c455
Marat Sharshenaliev <sharshenaliev.03@gmail.com>
2023-10-15 21:32:36 +0600

    changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView,
+    TokenObtainPairView
+)
 from accounts import views
 
 
 urlpatterns = [
     path("register/", views.UserRegistrationAPIView.as_view(), name="create-user"),
-    path("login/", views.UserLoginAPIView.as_view(), name="login-user"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path('user/', views.UserAPIView.as_view(), name="user"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
+
 ]
